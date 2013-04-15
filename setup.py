37257f154edfde862910837c3b6d60f1d5a7e60d
Marco Scheidhuber <j23d@jusid.de>
2013-04-15 11:20:59 +0200

    Depend on newest Kotti.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Kotti>=0.8',
+        'Kotti>=0.9a1',
     ],
     entry_points={
         'fanstatic.libraries': [
